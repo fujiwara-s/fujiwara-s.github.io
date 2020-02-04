dfc77751ca511ff11ab4ce404803ee6abe254b3f
fujiwara-s <60666310+fujiwara-s@users.noreply.github.com>
2020-02-05 02:57:55 +0900

    Create highAndLowS01.php

---
+++ highAndLowS01.php
@@ -0,0 +1,20 @@
+<html>
+   <center>
+	<head>
+		<meta http-equiv="Content-Type"content="text/html;charset=UTF-8">
+	</head>
+	<body>
+		<font size = "6">High & LOWゲーム</font><hr>
+
+<!-- 		0～13のランダム数値取得 -->
+		<?php
+		$leftCards = mt_rand(0,13);
+		echo "ランダム数値：",$leftCards;
+		?>
+	</body>
+   </center>
+</html>
+
+<head>
+	<meta http-equiv="Content-Type"content="text/html;charset=UTF-8">
+</head>
